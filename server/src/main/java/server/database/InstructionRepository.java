fa268918336e022f3918a879a88912c32528a5ba
Daphne Charaki <d.charaki@student.tudelft.nl>
2025-12-04 11:54:23 +0100

    Creation of InstructionRepository

--- server/src/main/java/server/database/InstructionRepository.java
+++ server/src/main/java/server/database/InstructionRepository.java
@@ -2,7 +2,6 @@ package server.database;
 
 import commons.Instruction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface InstructionRepository extends JpaRepository<Instruction, Long> {
 
