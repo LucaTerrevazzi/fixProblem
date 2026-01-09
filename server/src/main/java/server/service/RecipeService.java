f1a15e58f4a894b29c17f3b01a9ff893066a8909
Bo Li <b.li-23@student.tudelft.nl>
2026-01-09 18:52:25 +0100

    Fixing merge conflicts

--- server/src/main/java/server/service/RecipeService.java
+++ server/src/main/java/server/service/RecipeService.java
@@ -122,7 +122,7 @@ public class RecipeService {
      * delete a recipe by id.
      * @param id the id of the recipe to delete
      */
-    public void deleteById(long id) {
+    public void delete(long id) {
         recipeRepo.deleteById(id);
     }
 }
