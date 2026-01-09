a8bd73f6d1bc3dae083ff2d900c9b3328d6ba144
Karsten van den Heuvel <K.C.Heuvel@student.tudelft.nl>
2026-01-09 21:26:45 +0100

    created ingredient adder window

--- client/src/main/java/client/scenes/AddIngredientCtrl.java
+++ client/src/main/java/client/scenes/AddIngredientCtrl.java
@@ -1,20 +1,65 @@
 package client.scenes;
 
+import client.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.Ingredient;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class AddIngredientCtrl {
-    @FXML
-    private Button toOverviewButton;
+
+    @FXML private Label errorLabel;
+    @FXML private TextField nameField;
+    @FXML private TextField fatField;
+    @FXML private TextField proteinField;
+    @FXML private TextField carbsField;
 
     private FoodPalCtrl pc ;
     @Inject
     public AddIngredientCtrl(FoodPalCtrl p) {
         this.pc = p ;
     }
-    public void click() {
-        System.out.println(" Go to Ingredient overview scene ");
+
+    @FXML
+    public void goBack() {
         pc.showIngredientOverview();
+    }
+
+    @FXML
+    public void createIngredient() {
+        System.out.println("Creating ingredient ...");
+        errorLabel.setText("");
+
+        String name = nameField.getText().trim();
+        String fat = fatField.getText().trim();
+        String protein = proteinField.getText().trim();
+        String carbs = carbsField.getText().trim();
+        if (name.isEmpty() || fat.isEmpty() || protein.isEmpty() || carbs.isEmpty()) {
+            errorLabel.setText("Please fill in all of the fields.");
+            return;
+        }
+
+        try {
+            double f = Double.parseDouble(fat);
+            double p = Double.parseDouble(protein);
+            double c = Double.parseDouble(carbs);
+            if (f < 0 || p < 0 || c < 0) {
+                errorLabel.setText("Only positive numbers are allowed.");
+            }
+            Ingredient ingredient = new Ingredient(name);
+            ingredient.setFat(f);
+            ingredient.setProtein(p);
+            ingredient.setCarbs(c);
+            ServerUtils.addIngredient(ingredient);
+        }
+        catch (NumberFormatException e) {
+            errorLabel.setText("Please make sure all numbers are formatted properly (e.g. 0.0 instead of 0,0).");
+            return;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
