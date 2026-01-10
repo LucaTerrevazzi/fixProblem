0688c36ec958ed3788f4d2fc022d25ba94f30b7a
Melchior Besançon <m.besancon@student.tudelft.nl>
2026-01-10 09:00:16 +0100

    Added a few tests for the language method.

---
+++ commons/src/test/java/commons/LanguageTest.java
@@ -0,0 +1,35 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LanguageTest {
+
+    @Test
+    void testEnglishExpandedForm() {
+        assertEquals("English", Language.EN.getExpandedForm(), "EN should expand to English");
+    }
+
+    @Test
+    void testDutchExpandedForm() {
+        assertEquals("Dutch", Language.NL.getExpandedForm(), "NL should expand to Dutch");
+    }
+
+    @Test
+    void testGermanExpandedForm() {
+        assertEquals("German", Language.GR.getExpandedForm(), "GR should expand to German");
+    }
+
+    @Test
+    void testAllValuesNotNull() {
+        for (Language lang : Language.values()) {
+            assertNotNull(lang.getExpandedForm(), lang + " expanded form should not be null");
+        }
+    }
+
+    @Test
+    void testEnumContainsAllExpected() {
+        Language[] expected = {Language.EN, Language.NL, Language.GR};
+        assertArrayEquals(expected, Language.values(), "Language enum should contain EN, NL, GR");
+    }
+}
